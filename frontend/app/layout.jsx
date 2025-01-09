ae14132eb22f09195444a7ec519f600bfe961894
refh96 <rodrigo.flores1501@alumnos.ubiobio.cl>
2025-01-09 01:50:48 -0300

    separacion de responsabilidades del layout

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,32 +1,19 @@
 // app/layout.js
-'use client';
-import "./globals.css";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
-
+import './globals.css';
+import ClientLayout from '../ClientLayout';
+// Define las propiedades de metadata (funciona solo en componentes de servidor)
 export const metadata = {
-  title: "Full Wash Conce", // Cambia el título global aquí
-  description: "Bienvenido a Full Wash, tus servicios de lavado de vehículos.", // Descripción opcional
+  title: "Full Wash Conce",
+  description: "Bienvenido a Full Wash, tus servicios de lavado de vehículos.",
 };
 
-function Layout({ children }) {
-  // Crear una instancia de QueryClient
-  const [queryClient] = useState(() => new QueryClient());
-
+export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        <title>{metadata.title}</title>
         <link rel="icon" href="/favicon.png" />
       </head>
-      <body>
-        {/* Proveer el QueryClient a la aplicación */}
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </body>
+      <ClientLayout>{children}</ClientLayout>
     </html>
   );
 }
-
-export default Layout;
