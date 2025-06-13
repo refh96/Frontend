6e78b2863d649f3179bb9d143c953d4223e8cc32
refh96 <rodrigo.flores1501@alumnos.ubiobio.cl>
2025-06-13 02:51:08 -0400

    agregando adsense a layout

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,9 +1,10 @@
 // app/layout.js
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
-  title: "Full Wash Conce", // Cambia el título global aquí
-  description: "Bienvenido a Full Wash, tus servicios de lavado de vehículos.", // Descripción opcional
+  title: "Full Wash Conce",
+  description: "Bienvenido a Full Wash, tus servicios de lavado de vehículos.",
 };
 
 function Layout({ children }) {
@@ -12,6 +13,14 @@ function Layout({ children }) {
       <head>
         <title>{metadata.title}</title>
         <link rel="icon" href="/favicon.png" />
+        {/* Fragmento oficial de Google AdSense */}
+        <Script
+          id="adsense-script"
+          strategy="beforeInteractive"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7870558147553980"
+          crossOrigin="anonymous"
+        />
       </head>
       <body>{children}</body>
     </html>
